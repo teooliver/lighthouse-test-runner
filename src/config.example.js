57e3196ca5dbb7292fd2630399fe31cd5c98cba0
Teo Oliver <teo.liz.oli@gmail.com>
2021-08-18 16:58:35 +0100

    add: better documentation

--- src/config.example.js
+++ src/config.example.js
@@ -3,7 +3,9 @@ module.exports = {
     controlURL: 'https://www.xxx.com/uk',
     testURL: 'https://www.yyy.com/uk',
   },
+  // Ex: Final file name will be: branch-1234-control-v1.report.html
   outFileName: 'branch-1234',
   reportsFolder: 'reports',
+  // Amount of tests
   runLimit: 10,
 };
